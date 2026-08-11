4c43869d2e128a65a67fc4f82f3676c06d0bc643
frank-fan-feng <118092528+frank-fan-feng@users.noreply.github.com>
2026-08-11 05:34:07 +0000

    Deploying to gh-pages from @ frank-fan-feng/frank-fan-feng.github.io@8cc464f66359e885e30d4a223e6afc2ec3c0ead2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-semi-supervised-learning-approach-to-handling-missing-data-in-predictive-analytics-received-best-paper-in-track-award-at-the-2024-pacific-asia-conference-on-information-systems-ho-chi-minh-city-vietnam",
+        },{id: "news-a-semi-supervised-learning-approach-to-handling-missing-data-in-predictive-analytics-received-best-paper-in-track-award-at-the-2024-pacific-asia-conference-on-information-systems-ho-chi-minh-city-vietnam",
           title: '“A semi-supervised learning approach to handling missing data in predictive analytics” received Best...',
           description: "",
           section: "News",},{id: "news-beijing-municipal-social-science-foundation-grant-on-generative-ai-and-supply-chain-resilience",
